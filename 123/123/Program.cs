dafad1d7b97a9661f4d230ede3df4046ab073998
Evgenia18 <118425098+Evgenia18@users.noreply.github.com>
2022-11-29 15:16:20 +0900

    Add files via upload

---
+++ 123/123/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zxc
+{
+    internal class qwe
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int result;
+            if (n > 123)
+                result = Math.Abs((n - 123) * 3);
+            else
+                result = Math.Abs(123 - n);
+            Console.WriteLine(result);
+        }
+    }
+}
